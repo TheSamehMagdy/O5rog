27d1a2b2bb0de44ca3fbd0eaec25d6252f9462b0
Sameh Magdy <thesamehmagdy@gmail.com>
2018-09-11 14:53:39 +0000

    Add middleware for checking comment ownership

--- middleware/index.js
+++ middleware/index.js
@@ -1,4 +1,5 @@
-var Place = require("../models/place");
+var Place   = require("../models/place");
+var Comment = require("../models/comment");
 
 var middlewareObj = {};
 
@@ -24,6 +25,27 @@ middlewareObj.checkPlaceOwnership = function(req, res, next) {
     }
 };
 
+middlewareObj.checkCommentOwnership = function(req, res, next) {
+ if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id, function(err, foundComment){
+           if(err || !foundComment){
+               req.flash("error", "Comment not found.");
+               res.redirect("back");
+           } else {
+               // does user own the comment?
+            if(foundComment.author.id.equals(req.user._id)) {
+                next();
+            } else {
+                req.flash("error", "You don't have permission to do that.");
+                res.redirect("back");
+            }
+           }
+        });
+    } else {
+        req.flash("error", "You need to be logged in to do that.");
+        res.redirect("back");
+    }
+};
 
 middlewareObj.isLoggedIn = function(req, res, next){
     if(req.isAuthenticated()){
